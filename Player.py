18220f3eecdb3618d8fba824863fa48d86ad5a31
Pibaud <thibaud.paulin@gmail.com>
2024-06-05 18:50:51 +0200

    "Updated Player.py and Skyjo.py files with new methods and logic, and modified existing code."

--- Player.py
+++ Player.py
@@ -4,21 +4,57 @@ class Player:
         self.knownHand = [['?' for _ in range(4)] for _ in range(3)]  # Initialiser la main connue avec des "?"
         self.name = name
         self.score = 0
+        self.hasFinished = False
 
     def __str__(self):
         res = f"{self.name}:\n"
-        for ligne in self.knownHand:  # Pour chaque sous-liste de la main connue
-            chaine_ligne = ""
-            for element in ligne:
-                chaine_ligne += f"{element:>5}"  # Formater les éléments avec 5 espaces
-            res += chaine_ligne + "\n"
+        for row in self.knownHand:  # Pour chaque sous-liste de la main connue
+            string = ""
+            for element in row:
+                string += f"{element:>5}"  # Formater les éléments avec 5 espaces
+            res += string + "\n"
         return res
     
+    def setHasFinished(self):
+        self.hasFinished = True
+        
+    def resetHasFinished(self):
+        self.hasFinished = False
+        
+    def revealAll(self):
+        self.knownHand = self.realHand
+    
     def cardSelection(self):
+        test = "wasKnown"
         line = int(input("Entrez la ligne de la carte\n"))-1
         column = int(input("Entrez la colonne de la carte\n"))-1
+        if(self.knownHand[line][column] == "?"):
+            test = "wasUnknown"
         self.knownHand[line][column] = self.realHand[line][column]
-        return (self.knownHand[line][column], (line, column))
+        return (self.knownHand[line][column], (line, column), test)
+    
+    def revealCheck(self):
+        return all(cell != "?" for row in self.knownHand for cell in row)
+    
+    def columnTest(self, discard):
+        for col in range(4):
+            first_card = self.knownHand[0][col]
+            all_same = True
+            for row in range(1, 3):
+                if(self.knownHand[row][col] != first_card):
+                    all_same = False
+                    break
+            if (all_same):
+                for row in range(3):
+                    discard.append(self.knownHand[row][col])
+                    del self.knownHand[row][col]
+                print(f"Vous avez une colonne complète de {first_card} ! Elle est enlevée de votre jeu")
+        return discard
+    
+    def calculateScore(self):
+        for row in self.knownHand:
+            for element in row:
+                self.score += element
 
     def twoFirstCardsSelection(self):
         print(f"{self.name}, Choisissez deux cartes de votre choix dans votre jeu\n")
@@ -35,14 +71,47 @@ class Player:
         return (self.knownHand[lineFirst][columnFirst], self.knownHand[lineSecond][columnSecond])
     
     def play(self, draw, discard, seenCards):
+        print(f"Je sais que les cartes qui ont été jouées sont :\n{seenCards}\n")
         choice = int(input()) # Mettre l'IA ici
-        while(choice != 1 or choice != 2):
+        while(choice != 1 and choice != 2):
                 choice = int(input("Erreur, veuillez choisir 1 ou 2 :\n"))
         if(choice == 1):
-            print(f"Vous prenez la carte {discard[len(discard)-1]} de la pile\nAvec quelle carte de votre main voulez-vous l'échanger ?\n")
-            print(self)
+            print(f"Vous prenez la carte {discard[-1]} de la pile\nAvec quelle carte de votre main voulez-vous l'échanger ?\n")
             cardAndCoord = self.cardSelection()
             print(f"Vous échangez un {cardAndCoord[0]} contre un {discard[len(discard)-1]}")
             self.knownHand[cardAndCoord[1][0]][cardAndCoord[1][1]] = discard[len(discard)-1]
-            seenCards.append(cardAndCoord[0])
-            discard.append
+            self.realHand[cardAndCoord[1][0]][cardAndCoord[1][1]] = discard[len(discard)-1]
+            discard = discard[:len(discard)-2]
+            discard.append(cardAndCoord[0])
+            if(cardAndCoord[2] == "wasUnknown"):
+                seenCards.append(cardAndCoord[0])
+        else:
+            print(f"Vous piochez\nLa carte obtenue est un {draw[0]}\nVoulez-vous l'échanger avec une de vos cartes (entrez '1') ou la poser sur la défausse et retourner une de vos cartes cachées (entrez '2') ?\n")
+            seenCards.append(draw[0])
+            choiceTwo = int(input())
+            while(choiceTwo != 1 and choice != 2):
+                choiceTwo = int(input("Erreur, veuillez choisir 1 ou 2 :\n"))
+            if(choiceTwo == 1):
+                cardAndCoord = self.cardSelection()
+                print(f"Vous échangez un {cardAndCoord[0]} contre un {draw[0]}")
+                self.knownHand[cardAndCoord[1][0]][cardAndCoord[1][1]] = draw[0]
+                self.realHand[cardAndCoord[1][0]][cardAndCoord[1][1]] = draw[0]
+                discard.append(cardAndCoord[0])
+                if(cardAndCoord[2] == "wasUnknown"):
+                    seenCards.append(cardAndCoord[0])
+            else:
+                print(f"Vous posez la carte {draw[0]} sur la défausse")
+                discard.append(draw[0])
+                draw = draw[1:]
+                flippedCard = self.cardSelection()
+                while(flippedCard[2] != "wasUnknown"):
+                    print(f"La carte doit être inconnue\n")
+                    flippedCard = self.cardSelection()
+                print(f"Vous retournez une carte, c'est un {flippedCard[0]}")
+        print(f"Votre jeu : {self}")
+        discard = self.columnTest()
+        if(self.revealCheck()):
+            print(f"{self.name} a retourné sa dernière carte ! C'est le dernier tour pour les autres joueurs !")
+            self.setHasFinished()
+        self.calculateScore()
+        return(draw, discard, seenCards)
--- Skyjo.py
+++ Skyjo.py
@@ -1,21 +1,15 @@
 import numpy as np
-from Player import Player  # Importer correctement la classe Player
+from Player import Player
 
 class Skyjo:
     # Se déroule en plusieurs manches et se termine dès qu'un joueur atteint 100 points ou plus. Celui qui a obtenu le moins de points gagne la partie.
     def __init__(self, playersList):
-        self.draw = np.random.permutation(list(np.fromiter([-2] * 5 + [0] * 15 + [nombre for nombre in range(-1, 13) for _ in range(10)], dtype=int)))
-        self.playersList = []  # Utiliser une liste pour les joueurs (plus flexible)
+        self.draw = np.array([])
+        self.namesList = playersList
+        self.playersList = []
         self.discard = []
-        for name in playersList:
-            hand = []
-            for _ in range(3):  # Distribuer 3 ensembles de 4 cartes par joueur
-                hand.append(self.draw[:4].tolist())  # Distribuer efficacement 4 cartes et retirer du paquet
-                self.draw = self.draw[4:]
-            newPlayer = Player(hand, name)
-            self.playersList.append(newPlayer)  # Ajouter les objets Joueur à la liste
-        self.seenCards = np.array([])
-        self.playerTurn = self.playersList[0]
+        self.seenCards = []
+        self.playerTurn = None
 
     def displayHands(self):
         for player in self.playersList:
@@ -24,50 +18,86 @@ class Skyjo:
     def displayPlayerHand(self, player):
         print(player)
         
-    def reorderPlayers(players, startingPlayer):
-        return players[players.index(startingPlayer):] + players[:players.index(startingPlayer)]
+    def reorderPlayers(self):
+        return self.playersList[self.playersList.index(self.playerTurn):] + self.playersList[:self.playersList.index(self.playerTurn)]
     
     def unpileDraw(self):
         unpiledCard = self.draw[0]
         self.seenCards.append(unpiledCard)
         self.discard.append(unpiledCard)
         return 
+    
+    def displayAllRealHands(self):
+        for player in self.playersList:
+            player.revealAll()
+        self.displayHands()
             
     def jouerPartie(self):
-        print("Bienvenue dans le Skyjo !\nDistribution des cartes :\n")
+        print("Bienvenue dans le Skyjo !\nDistribution des cartes...\n")
         self.displayHands()
-        biggestPlayerDelta = ("x", -10) # Choix du premier à jouer
-        for player in self.playersList:
-            couple = player.twoFirstCardsSelection()
-            self.seenCards.append(couple[0])
-            self.seenCards.append(couple[1])
-            self.displayPlayerHand(player)
-            if(abs(couple[0]-couple[1]) > biggestPlayerDelta[1]):
-                biggestPlayerDelta = (player, abs(couple[0]-couple[1]))
-        print("Voici les cartes choisies par tout le monde :\n")
-        self.displayHands()
-        self.playerTurn = biggestPlayerDelta[0]
-        print(f"{self.playerTurn.name} a la plus grande différence de cartes et commence à jouer !\n")
-        self.playersList = self.reorderPlayers(self.playersList, self.playerTurn)
-        print(f"Ordre des joueurs : {self.playersList}")
         while all(player.score < 100 for player in self.playersList):
+            biggestPlayerSum = ("x", -10) # Choix du premier à jouer
+            for player in self.playersList:
+                couple = player.twoFirstCardsSelection()
+                self.seenCards.append(couple[0])
+                self.seenCards.append(couple[1])
+                self.displayPlayerHand(player)
+                if(couple[0]+couple[1] > biggestPlayerSum[1]):
+                    biggestPlayerSum = (player, couple[0]+couple[1])
+            print("Voici les cartes choisies par tout le monde :\n")
+            self.displayHands()
+            self.playerTurn = biggestPlayerSum[0]
+            print(f"{self.playerTurn.name} a la plus grande somme de cartes et commence à jouer !\n")
+            self.playersList = self.reorderPlayers()
+            print(f"Ordre des joueurs : {self.playersList}")
             self.playRound()
+            self.displayScores()
+            for player in self.playersList:
+                player.resetHasFinished()
 
-    def playRound(self): #faire commencer à jouer le bon joueur stocké dans self.playerTurn
-        currentPlayer = self.playerTurn
-        for player in self.playersList:
-            for notYou in self.playersList:
-                if(notYou.name != player.name):
-                    print(f"Jeu de {notYou.name} : \n {notYou}")
-            print(f"Carte de la pile : {self.draw[0]}\nVotre jeu : \n {player}\nVotre score : {player.score}\nVoulez vous prendre la carte de la pile (entrez '1') ou une carte de la pioche (entrez '2') ?\nVotre choix : ")
-            player.play(self)#donner toutes les données du jeu
-                
+    def playRound(self):
+        self.draw = np.random.permutation(list(np.fromiter([-2] * 5 + [0] * 15 + [nombre for nombre in range(-1, 13) for _ in range(10)], dtype=int)))
+        self.discard = [self.draw[0]]
+        for name in self.namesList:
+            hand = []
+            for _ in range(3):  # Distribuer 3 ensembles de 4 cartes par joueur
+                hand.append(self.draw[:4].tolist())  # Distribuer efficacement 4 cartes et retirer du paquet
+                self.draw = self.draw[4:]
+            newPlayer = Player(hand, name)
+            self.playersList.append(newPlayer)  # Ajouter les objets Joueur à la liste
+        self.seenCards = [self.discard[0]]
+        self.playerTurn = self.playersList[0]
+        while(True):
+            for player in self.playersList:
+                for notYou in self.playersList:
+                    if(notYou.name != player.name):
+                        print(f"Jeu de {notYou.name} :\n {notYou}")
+                print(f"Carte de la pile : {self.discard[-1]}\nVotre jeu : \n {player}\nVotre score : {player.score}\nVoulez vous prendre la carte de la pile (entrez '1') ou une carte de la pioche (entrez '2') ?\nVotre choix : ")
+                newGameState = player.play(self.draw, self.discard, self.seenCards)
+                self.draw = newGameState[0]
+                self.discard = newGameState[1]
+                self.seenCards = newGameState[2]
+                if(player.hasFinished):
+                    self.playerTurn = player
+                    remainingPlayers = self.reorderPlayers()
+                    remainingPlayers = remainingPlayers[1:] # on obtient la liste des joueurs qui doivent finir de jouer
+                    for remainingPlayer in remainingPlayers :
+                        for notYou in remainingPlayers:
+                            if(notYou.name != remainingPlayer.name):
+                                print(f"Jeu de {notYou.name} :\n {notYou}")
+                        print(f"Carte de la pile : {self.discard[-1]}\nVotre jeu : \n {remainingPlayer}\nVotre score : {remainingPlayer.score}\nVoulez vous prendre la carte de la pile (entrez '1') ou une carte de la pioche (entrez '2') ?\nVotre choix : ")
+                        newGameState = remainingPlayer.play(self.draw, self.discard, self.seenCards)
+                        self.draw = newGameState[0]
+                        self.discard = newGameState[1]
+                        self.seenCards = newGameState[2]
+                    print(f"C'est fini ! Tout le monde retourne ses cartes !")
+                    self.displayAllRealHands()
+                    return 0
 
-    def afficher_scores(self):
+    def displayScores(self):
         for player in self.playersList:
             print(f"Score de {player.name} : {player.score}")
 
-# Définir les joueurs et initialiser le jeu
 players = ["Alice", "Bob"]
 game = Skyjo(players)
 game.jouerPartie()
