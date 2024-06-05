e0c2ec4b2e0ed9a8ae80b3d4d1af576ff4b0cdc2
Pibaud <thibaud.paulin@gmail.com>
2024-06-05 13:14:46 +0200

    "Updated Player and Skyjo classes with new methods and functionality, including card selection and gameplay logic."

--- Player.py
+++ Player.py
@@ -13,18 +13,36 @@ class Player:
                 chaine_ligne += f"{element:>5}"  # Formater les éléments avec 5 espaces
             res += chaine_ligne + "\n"
         return res
+    
+    def cardSelection(self):
+        line = int(input("Entrez la ligne de la carte\n"))-1
+        column = int(input("Entrez la colonne de la carte\n"))-1
+        self.knownHand[line][column] = self.realHand[line][column]
+        return (self.knownHand[line][column], (line, column))
 
     def twoFirstCardsSelection(self):
-        print("Choisissez deux cartes de votre choix dans votre jeu\n")
-        lineFirst = int(input("Entrez la ligne de la première carte"))-1
-        columnFirst = int(input("Entrez la colonne de la première carte"))-1
+        print(f"{self.name}, Choisissez deux cartes de votre choix dans votre jeu\n")
+        lineFirst = int(input("Entrez la ligne de la première carte\n"))-1
+        columnFirst = int(input("Entrez la colonne de la première carte\n"))-1
         self.knownHand[lineFirst][columnFirst] = self.realHand[lineFirst][columnFirst]
-        lineSecond = int(input("Entrez la ligne de la deuxième carte"))-1
-        columnSecond = int(input("Entrez la colonne de la deuxième carte"))-1
+        lineSecond = int(input("Entrez la ligne de la deuxième carte\n"))-1
+        columnSecond = int(input("Entrez la colonne de la deuxième carte\n"))-1
         while(lineFirst == lineSecond and columnFirst == columnSecond):
-            print("Veuillez choisir une carte différente de la première")
-            lineSecond = int(input("Entrez la ligne de la deuxième carte"))-1
-            columnSecond = int(input("Entrez la colonne de la deuxième carte"))-1
+            print("Veuillez choisir une carte différente de la première\n")
+            lineSecond = int(input("Entrez la ligne de la deuxième carte\n"))-1
+            columnSecond = int(input("Entrez la colonne de la deuxième carte\n"))-1
         self.knownHand[lineSecond][columnSecond] = self.realHand[lineSecond][columnSecond]
-        return ((self.knownHand[lineFirst][columnFirst], self.knownHand[lineSecond][columnSecond]), self.knownHand)
-        
+        return (self.knownHand[lineFirst][columnFirst], self.knownHand[lineSecond][columnSecond])
+    
+    def play(self, draw, discard, seenCards):
+        choice = int(input()) # Mettre l'IA ici
+        while(choice != 1 or choice != 2):
+                choice = int(input("Erreur, veuillez choisir 1 ou 2 :\n"))
+        if(choice == 1):
+            print(f"Vous prenez la carte {discard[len(discard)-1]} de la pile\nAvec quelle carte de votre main voulez-vous l'échanger ?\n")
+            print(self)
+            cardAndCoord = self.cardSelection()
+            print(f"Vous échangez un {cardAndCoord[0]} contre un {discard[len(discard)-1]}")
+            self.knownHand[cardAndCoord[1][0]][cardAndCoord[1][1]] = discard[len(discard)-1]
+            seenCards.append(cardAndCoord[0])
+            discard.append
--- Skyjo.py
+++ Skyjo.py
@@ -6,6 +6,7 @@ class Skyjo:
     def __init__(self, playersList):
         self.draw = np.random.permutation(list(np.fromiter([-2] * 5 + [0] * 15 + [nombre for nombre in range(-1, 13) for _ in range(10)], dtype=int)))
         self.playersList = []  # Utiliser une liste pour les joueurs (plus flexible)
+        self.discard = []
         for name in playersList:
             hand = []
             for _ in range(3):  # Distribuer 3 ensembles de 4 cartes par joueur
@@ -13,40 +14,60 @@ class Skyjo:
                 self.draw = self.draw[4:]
             newPlayer = Player(hand, name)
             self.playersList.append(newPlayer)  # Ajouter les objets Joueur à la liste
-        self.playedCards = np.array([])
+        self.seenCards = np.array([])
         self.playerTurn = self.playersList[0]
 
-    def afficher_mains(self):
+    def displayHands(self):
         for player in self.playersList:
             print(player)
-
+            
+    def displayPlayerHand(self, player):
+        print(player)
+        
+    def reorderPlayers(players, startingPlayer):
+        return players[players.index(startingPlayer):] + players[:players.index(startingPlayer)]
+    
+    def unpileDraw(self):
+        unpiledCard = self.draw[0]
+        self.seenCards.append(unpiledCard)
+        self.discard.append(unpiledCard)
+        return 
+            
     def jouerPartie(self):
+        print("Bienvenue dans le Skyjo !\nDistribution des cartes :\n")
+        self.displayHands()
         biggestPlayerDelta = ("x", -10) # Choix du premier à jouer
         for player in self.playersList:
             couple = player.twoFirstCardsSelection()
-            # révéler les cartes choisies
+            self.seenCards.append(couple[0])
+            self.seenCards.append(couple[1])
+            self.displayPlayerHand(player)
             if(abs(couple[0]-couple[1]) > biggestPlayerDelta[1]):
-                biggestPlayerDelta = (player.name, abs(couple[0]-couple[1]))
+                biggestPlayerDelta = (player, abs(couple[0]-couple[1]))
+        print("Voici les cartes choisies par tout le monde :\n")
+        self.displayHands()
         self.playerTurn = biggestPlayerDelta[0]
-        print(f"{self.playerTurn.name} commence à jouer !\n")
+        print(f"{self.playerTurn.name} a la plus grande différence de cartes et commence à jouer !\n")
+        self.playersList = self.reorderPlayers(self.playersList, self.playerTurn)
+        print(f"Ordre des joueurs : {self.playersList}")
         while all(player.score < 100 for player in self.playersList):
-            self.jouerManche()
-            self.afficher_scores()
+            self.playRound()
 
-    def jouerManche(self):
+    def playRound(self): #faire commencer à jouer le bon joueur stocké dans self.playerTurn
+        currentPlayer = self.playerTurn
         for player in self.playersList:
             for notYou in self.playersList:
                 if(notYou.name != player.name):
                     print(f"Jeu de {notYou.name} : \n {notYou}")
-            print(f"Votre jeu : \n {player}")
-            print(f"Carte de la pile : {self.draw[0]}")
-            print(f"Score de {player.name} : {player.score}")
+            print(f"Carte de la pile : {self.draw[0]}\nVotre jeu : \n {player}\nVotre score : {player.score}\nVoulez vous prendre la carte de la pile (entrez '1') ou une carte de la pioche (entrez '2') ?\nVotre choix : ")
+            player.play(self)#donner toutes les données du jeu
+                
 
     def afficher_scores(self):
         for player in self.playersList:
             print(f"Score de {player.name} : {player.score}")
 
 # Définir les joueurs et initialiser le jeu
-players = ["Alice", "Bob", "Charlie", "David"]
+players = ["Alice", "Bob"]
 game = Skyjo(players)
 game.jouerPartie()
